e9a0ffb27b02e50611b590c6f56141458a041c6a
Jijidici <jdefaye.jijidici@gmail.com>
2013-01-04 22:44:01 +0100

    Add addPoint() method to PointList class

--- include/PointList.hpp
+++ include/PointList.hpp
@@ -12,6 +12,7 @@ namespace leydef{
 			PointList(uint32_t color);
 			~PointList();
 			int getSize() const;
+			void addPoint(const Eigen::VectorXd& newPoint);
 		
 		private:
 			std::vector<Eigen::VectorXd> m_points;
--- src/PointList.cpp
+++ src/PointList.cpp
@@ -1,11 +1,19 @@
 #include "PointList.hpp"
 
+#include <vector>
+#include <stdint.h>
+#include <Eigen/Dense>
+
 namespace leydef{
 	
 	PointList::PointList(uint32_t color) : m_color(color){
 	}
 	
 	PointList::~PointList(){
+	}
+	
+	void PointList::addPoint(const Eigen::VectorXd& newPoint){
+		m_points.push_back(newPoint);
 	}
 	
 	int PointList::getSize() const{
